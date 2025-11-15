f3fbd469965627661567c78fe3c4809106eaeca1
allter <adam.nowak789@onet.pl>
2025-11-15 16:20:35 +0100

    fix: added required file modules

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from scrappers import olx_scrapper
 
 app = FastAPI()
 
-URL = 'https://www.olx.pl/praca/zawiercie/?search%5Bdist%5D=30&search%5Bfilter_enum_type%5D%5B0%5D=halftime&search%5Bfilter_enum_type%5D%5B1%5D=seasonal&search%5Bfilter_enum_type%5D%5B2%5D=parttime&search%5Bfilter_enum_special_requirements%5D%5B0%5D=student_status&search%5Bfilter_enum_agreement%5D%5B0%5D=zlecenie&search%5Bfilter_enum_agreement%5D%5B1%5D=practice'
+#URL = 'https://www.olx.pl/praca/zawiercie/?search%5Bdist%5D=30&search%5Bfilter_enum_type%5D%5B0%5D=halftime&search%5Bfilter_enum_type%5D%5B1%5D=seasonal&search%5Bfilter_enum_type%5D%5B2%5D=parttime&search%5Bfilter_enum_special_requirements%5D%5B0%5D=student_status&search%5Bfilter_enum_agreement%5D%5B0%5D=zlecenie&search%5Bfilter_enum_agreement%5D%5B1%5D=practice'
+URL = 'https://www.olx.pl/praca/zawiercie/?search%5Bdist%5D=30'
+
+#TODO: maksium obecnie to 52 oferty - lazy loading obejscie
 
 @app.get('/')
 def root():
